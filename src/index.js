7ec5b4d61d1fb53887d6df1658c5940924b9996c
Jeff <jeff@imginconline.com>
2018-06-06 20:48:14 -0400

    authorize action was missing from index.js.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import auth from "./auth"
 import { auth0AuthorizeEpic, auth0LogoutEpic, auth0CallbackEpic, auth0RenewTokenEpic, auth0AuthenticatedEpic } from "./epics";
-import { auth0Init, auth0Login, auth0Logout, auth0LoginSuccess, auth0LoginError, auth0Authenticated, AUTH0_ERROR } from "./actions";
+import { authorize, auth0Init, auth0Login, auth0Logout, auth0LoginSuccess, auth0LoginError, auth0Authenticated, AUTH0_ERROR } from "./actions";
 import auth0Reducers from "./reducers";
 import sessionApi from "./session-api";
 
